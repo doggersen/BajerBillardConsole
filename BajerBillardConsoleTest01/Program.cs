f794215633d2c06609d4ea89429adb6cf21fa42c
doggersen <martin.mbj.johansen@gmail.com>
2021-12-21 21:05:58 +0100

    VStudio made a clever method, thats working. not sure if i should keep it.

--- BajerBillardConsoleTest01/Program.cs
+++ BajerBillardConsoleTest01/Program.cs
@@ -12,17 +12,33 @@ namespace BajerBillardConsoleTest01
         {
             List<Player> players = new List<Player>();
 
-            Console.WriteLine("skriv navn på spillere");
-            players.Add(new Player(Console.ReadLine()));
-            Console.WriteLine("skriv navn på spillere");
-            players.Add(new Player(Console.ReadLine()));
+            addPlayers(players);
 
             foreach (Player p in players)
-            { 
-                Console.WriteLine($"Navn: {p.Name}, Score: {p.Point} point"); 
+            {
+                Console.WriteLine($"Navn: {p.Name}, Score: {p.Point} point");
             }
 
+
             Console.ReadKey();
+        }
+
+        private static void addPlayers(List<Player> players)
+        {
+            bool morePlayers = true;
+            while (morePlayers)
+            {
+                Console.WriteLine("skriv navn på spiller");
+                players.Add(new Player(Console.ReadLine()));
+                Console.WriteLine("Tilføj flere spillere? skriv '1' for ja, '2' for nej.");
+                string userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    morePlayers = true;
+                }
+                else break;
+
+            }
         }
     }
 }
